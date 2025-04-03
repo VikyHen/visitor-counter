57972e6f00d9792f32691574604f3559a2675157
Viky Hendriko <vikyhendriko12@gmail.com>
2025-04-03 20:23:52 +0700

    update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const authRateLimiter = rateLimit({
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({ origin: ["http://localhost:5000", "http://10.0.2.2:5000", "visitor-counter-production.up.railway.app"], credentials: true }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -37,6 +37,6 @@ app.use('/api/auth',  authRateLimiter, authRouter);
 app.use('/api/user', userRouter);
 app.use('/api/visitor', visitorRouter);
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server started on port ${port}`);
 });
